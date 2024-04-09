f9c13fbe104311dfe42fa06c980d78ed6a779aab
Stonesword <stonesword503@gmail.com>
2024-04-08 21:46:35 -0400

    V0.1.2
    Added ship notifications when fuel or oxygen is low or depleted.

--- project1.cs
+++ project1.cs
@@ -9,6 +9,7 @@ using JetBrains.Annotations;
 using Mono.Cecil;
 using OWML.Common;
 using OWML.ModHelper;
+using Steamworks;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.SceneManagement;
@@ -25,7 +26,7 @@ public class project1 : ModBehaviour
 
 	private void Start()
 	{
-		///	I apologize for my inexperience in coding and commenting for readability
+		///	I apologize for my inexperience in good practices in code or comment readability
 		///	It's my first mod, don't judge too hard :]
 		ModHelper.Console.WriteLine($"{nameof(project1)}: Resource management loaded!", MessageType.Success);
 
@@ -88,6 +89,13 @@ public class project1 : ModBehaviour
 				{
 					mainElectricalSystem = mainElectricitySystemObject.GetComponent<ElectricalSystem>();
 				}
+
+				// Notification Display Data
+				// lowFuelNotification = new NotificationData(NotificationTarget.Ship, "WARNING: LOW FUEL", 1f, true); // Unused for now
+				criticalFuelNotification = new NotificationData(NotificationTarget.Ship, "WARNING: FUEL LEVEL CRITICAL", 1f, true);
+				lowOxygenNotification = new NotificationData(NotificationTarget.Ship, "WARNING: LOW OXYGEN", 1f, true);
+				criticalOxygenNotification = new NotificationData(NotificationTarget.Ship, "WARNING: OXYGEN LEVEL CRITICAL", 1f, true);
+				oxygenDepleted = new NotificationData(NotificationTarget.Ship, "OXYGEN DEPLETED", 1f, true);
 			}  	
 	    };
 	}
@@ -117,11 +125,14 @@ public class project1 : ModBehaviour
 	public static ElectricalSystem mainElectricalSystem;
 	public static LandingPadManager landingPadManager;
 	public static Locator locator;
-	public static Detector detector;	
+	public static Detector detector;
+	static public NotificationData lowFuelNotification;
+	static public NotificationData criticalFuelNotification;
+	static public NotificationData lowOxygenNotification;
+	static public NotificationData criticalOxygenNotification;
+	static public NotificationData oxygenDepleted;
 	public static float RefuelAmount = 0.2f;
 	public static float OxygenRefillAmount = 0.18f;
-	
-	
 
 	public class Functions
 	{
@@ -132,27 +143,63 @@ public class project1 : ModBehaviour
 			IsLanded = landingPadManager.IsLanded();	// Checks if ship is landed
 
 			//	Refueling and gauges manager
-			if (IsLanded)
+			// No fuel status
+			if (!IsLanded && fractionaryFuel > 0.3f)
+			{
+				shipFuelGauge._indicatorLight.SetEmissionColor(Color.black); // Turn light off when not refueling
+			}
+			// Refueling
+			else if (IsLanded)
 			{
 				shipResources.SetFuel(shipResources._currentFuel += RefuelAmount);	// Increase fuel when landed
 				shipFuelGauge._indicatorLight.SetEmissionColor(Color.green);	//	Turn gauge green light on when refueling
 			}
-			else if (0.0001f <= fractionaryFuel && fractionaryFuel <= 0.3f && !IsLanded)
+
+			// Critical fuel level
+			else if (0f < fractionaryFuel && fractionaryFuel <= 0.3f && !IsLanded)
 			{
 				shipFuelGauge._indicatorLight.SetEmissionColor(Color.red); // Turn on warning light when low fuel
 			}
-			else if (shipResources != null && !IsLanded && fractionaryFuel > 0.3f && !IsLanded)
-			{ 
-				shipFuelGauge._indicatorLight.SetEmissionColor(Color.black); // Turn light off when not refueling
+
+			// Ship's notification manager
+			// No status
+			if (fractionaryFuel > 0.3)
+			{
+				if (NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+				{
+					NotificationManager.SharedInstance.UnpinNotification(criticalFuelNotification);
+				}
+				else if (!NotificationManager.SharedInstance.IsPinnedNotification(criticalOxygenNotification))
+				{
+					return;
+				}
+			}
+
+			// Fuel level critical
+			else if (0f < fractionaryFuel && fractionaryFuel <= 0.3f)
+			{
+				if (!NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+				{
+					NotificationManager.SharedInstance.PostNotification(criticalFuelNotification, true);
+				}
+			}
+			
+			// Fuel depleted
+			else if (fractionaryFuel == 0)
+			{
+				if (NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+				{
+					NotificationManager.SharedInstance.UnpinNotification(criticalFuelNotification);
+				}
 			}
 
 			// Electrical systems & Refill station manager
-			if (shipResources._currentFuel <= 0)
+			if (shipResources._currentFuel <= 0f)
 			{
 				playerRecoveryPoint._refuelsPlayer = false;	// Disables refueling when ship is out of fuel
 				mainElectricalSystem.SetPowered(false);	// Disables electrical systems when ship is out of fuel
 			}
-			else if (shipResources._currentFuel > 0 && shipFuelGauge._shipDestroyed == false)
+			else if (shipResources._currentFuel > 0f && shipFuelGauge._shipDestroyed == false)
 			{
 				playerRecoveryPoint._refuelsPlayer = true;	// Activates refueling station in case of refueling
 				mainElectricalSystem.SetPowered(true);	// Enables electrical system in case of refueling
@@ -165,27 +212,63 @@ public class project1 : ModBehaviour
 			float fractionaryOxygen = shipResources.GetFractionalOxygen();	// Get fractional oxygen
 			bool isPlayerInShip = PlayerState.IsInsideShip();	// Check if player is inside ship
 
-			// Oxygen refill & Gauge lights manager
-			if (isOxygenAvailable)
-			{
-				shipResources.SetOxygen(shipResources._currentOxygen += OxygenRefillAmount);	// Increase oxygen if it's available
-				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.green);	// Turn gauge green light on when refilling
-			}
-			else if (0.001f <= fractionaryOxygen && fractionaryOxygen <= 0.16f && !isOxygenAvailable)
-			{
-				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.red);	// Turn red light on critical oxygen
-			}
-			else if (0.161 <= fractionaryOxygen && fractionaryOxygen <= 0.33f && !isOxygenAvailable)
-			{
-				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.yellow);	//Turn yellow light on low fuel
-			}
-			else if (!isOxygenAvailable && fractionaryOxygen > 0.33f)
+			// Oxygen refill & gauge lights manager
+			// No oxygen status
+			if (!isOxygenAvailable && fractionaryOxygen > 0.33f)
 			{ 
 				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.black); // Turn light off when oxygen is not available
 			}
 
+			// Refilling oxygen
+			else if (isOxygenAvailable)
+			{
+				shipResources.SetOxygen(shipResources._currentOxygen += OxygenRefillAmount);	// Increase oxygen if it's available
+				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.green);	// Turn gauge green light on when refilling
+			}
+
+			// Low oxygen
+			else if (0.16f < fractionaryOxygen && fractionaryOxygen <= 0.33f && !isOxygenAvailable)
+			{
+				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.yellow);
+			}
+
+			// Critical oxygen
+			else if (0f < fractionaryOxygen && fractionaryOxygen <= 0.16f && !isOxygenAvailable)
+			{
+				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.red);	// Turn red light on critical oxygen
+			}
+
+			// Notifications manager
+			// No condition:
+			if (fractionaryOxygen > 0.33f && NotificationManager.SharedInstance.IsPinnedNotification(lowOxygenNotification))
+			{
+				NotificationManager.SharedInstance.UnpinNotification(lowOxygenNotification);
+			}
+
+			// Low oxygen: 
+			else if (!NotificationManager.SharedInstance.IsPinnedNotification(lowOxygenNotification) && 0.16f < fractionaryOxygen && fractionaryOxygen <= 0.33f)
+			{
+				NotificationManager.SharedInstance.PostNotification(lowOxygenNotification, true);
+				NotificationManager.SharedInstance.UnpinNotification(criticalOxygenNotification);
+			}
+
+			// Critical condition:
+			else if (!NotificationManager.SharedInstance.IsPinnedNotification(criticalOxygenNotification) && 0f < fractionaryOxygen && fractionaryOxygen <= 0.16f)
+			{
+				NotificationManager.SharedInstance.PostNotification(criticalOxygenNotification, true);
+				NotificationManager.SharedInstance.UnpinNotification(lowOxygenNotification);
+				NotificationManager.SharedInstance.UnpinNotification(oxygenDepleted);
+			}
+
+			// Oxygen depleted
+			else if (!NotificationManager.SharedInstance.IsPinnedNotification(oxygenDepleted) && fractionaryOxygen == 0f)
+			{
+				NotificationManager.SharedInstance.PostNotification(oxygenDepleted, true);
+				NotificationManager.SharedInstance.UnpinNotification(criticalOxygenNotification);
+			}
+
 			// OxygenVolumes manager
-			if (shipResources._currentOxygen == 0 && playerHasOxygenVolume && isPlayerInShip || !isPlayerInShip)
+			if (shipResources._currentOxygen == 0f && playerHasOxygenVolume && isPlayerInShip || !isPlayerInShip)
 			{
 				playerOxygenDetector._activeVolumes.Remove(shipOxygenVolume); // Remove ship's OxygenVolume from player
 			}
