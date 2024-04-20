be9d04ce3bf60184b04e5b3182eb75fd3a3b3e48
Stonesword <stonesword503@gmail.com>
2024-04-19 22:20:03 -0400

    0.3.0
    - Added ship emergency thrusters
    - Added mod config

--- project1.cs
+++ project1.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Threading;
 using HarmonyLib;
+using JetBrains.Annotations;
 using OWML.Common;
 using OWML.ModHelper;
 using Steamworks;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 namespace project1;
 
@@ -33,14 +38,28 @@ public class project1 : ModBehaviour
 	static public NotificationData criticalFuelNotification;
 	static public NotificationData lowOxygenNotification;
 	static public NotificationData criticalOxygenNotification;
-	static public NotificationData usingEmergencyThrusters;
 	static public NotificationData oxygenDepleted;
 	static public ShipThrusterModel shipThrusterModel;
+	static public ThrusterModel thrusterModel;
 	static public bool isSeatedExitDelay;
 	static public bool isPlayerSeated = PlayerState.AtFlightConsole();
-	public static float RefuelAmount = 0.8f;
-	public static float OxygenRefillAmount = 0.7f;
+	public static bool isUsingEmergencyThrusters;
 
+	// Settings
+	// Oxygen settings
+	public static float oxygenRefillAmount;
+	public static float maxOxygen;
+	public static bool enableOxygenRegen;
+
+	// Fuel settings
+	public static float refuelAmount;
+	public static float maxFuel;
+	public static float landingPadRefuelMultiplier;
+	public static bool enableRefuel;
+
+	// Misc settings
+	public static bool useOxygenAsFuel;
+	public static float oxygenAsFuelMultiplier;
 	private void Awake()
 	{
 		Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
@@ -64,6 +83,12 @@ public class project1 : ModBehaviour
 					playerResources = playerBody.GetComponent<PlayerResources>();
 				}
 
+				GameObject modelRocket_Body = GameObject.Find("ModelRocket_Body");
+				if (modelRocket_Body != null)
+				{
+					thrusterModel = modelRocket_Body.GetComponent<ThrusterModel>();
+				}
+
 				GameObject shipBody = GameObject.Find("Ship_Body");
 				if (shipBody != null)
 				{
@@ -71,8 +96,13 @@ public class project1 : ModBehaviour
 					landingPadManager = shipBody.GetComponent<LandingPadManager>();
 					shipThrusterModel = shipBody.GetComponent<ShipThrusterModel>();
 
-					// Sets the max oxygen to 1080f units. This will make it so the ship oxyen lasts 18 minutes (This much time to account for player taking o2 from ship)
-					shipResources._maxOxygen = 1080;
+					// Sets the max oxygen to the desired value. It drains 1unit/s
+					shipResources._maxOxygen = maxOxygen;
+					shipResources._currentOxygen = maxOxygen;
+
+					// Sets the max fuel (For customizing purposes, it uses the vanilla value by default)
+					shipResources._maxFuel = maxFuel;
+					shipResources._currentFuel = maxFuel;
 				}
 
 				GameObject timberHearth_Body = GameObject.Find("TimberHearth_Body");
@@ -147,14 +177,33 @@ public class project1 : ModBehaviour
 				}
 
 				// Notification Display Data
+				shipResources._fuelDepletedNotification = null;
+				shipResources._fuelDepletedNotification = new NotificationData(NotificationTarget.Ship, "FUEL DEPLETED. USING OXYGEN AS FUEL", 1f, true);
 				// lowFuelNotification = new NotificationData(NotificationTarget.Ship, "WARNING: LOW FUEL", 1f, true); // Unused for now
 				criticalFuelNotification = new NotificationData(NotificationTarget.Ship, "WARNING: FUEL LEVEL CRITICAL", 1f, true);
 				lowOxygenNotification = new NotificationData(NotificationTarget.Ship, "WARNING: LOW OXYGEN", 1f, true);
 				criticalOxygenNotification = new NotificationData(NotificationTarget.Ship, "WARNING: OXYGEN LEVEL CRITICAL", 1f, true);
 				oxygenDepleted = new NotificationData(NotificationTarget.Ship, "OXYGEN DEPLETED", 1f, true);
-				//usingEmergencyThrusters = new NotificationData(NotificationTarget.Ship, "FUEL DEPLETED: USING OXYGEN AS FUEL", 1f, true); Not yet.
+				
 			}
 		};
+	}
+	public override void Configure(IModConfig config) 
+	{
+	//	// Oxygen settings
+		maxOxygen = config.GetSettingsValue<float>("Max Oxygen");
+		oxygenRefillAmount = config.GetSettingsValue<float>("Oxygen Regeneration Rate");
+		enableOxygenRegen = config.GetSettingsValue<bool>("Enable Oxygen Refill");
+
+		// Fuel settings
+		maxFuel = config.GetSettingsValue<float>("Max Fuel");
+		refuelAmount = config.GetSettingsValue<float>("Refuel Amount");
+		landingPadRefuelMultiplier = config.GetSettingsValue<float>("Landing Pad Refuel Multiplier");
+		enableRefuel = config.GetSettingsValue<bool>("Enable Refuel");
+
+		// Misc settings
+		useOxygenAsFuel = config.GetSettingsValue<bool>("Use Oxygen as Fuel");
+		oxygenAsFuelMultiplier = config.GetSettingsValue<float>("Oxygen as Fuel Multiplier");
 	}
 	private void FixedUpdate()
 	{
@@ -163,6 +212,7 @@ public class project1 : ModBehaviour
 		{
 			Functions.FuelManagement();
 			Functions.OxygenManagement();
+			Functions.OxygenAsThrusters();
 		}
 	}
 	private void OnEntry(GameObject collidedObject)
@@ -175,10 +225,11 @@ public class project1 : ModBehaviour
 
 	private void OnExit(GameObject collidedObject)
 	{
-		var body = collidedObject.GetAttachedOWRigidbody();
-		if (!body.CompareTag("Ship")) return;
-
-		isOnLandingPad = false;
+			var body = collidedObject.GetAttachedOWRigidbody();
+			if (body.CompareTag("Ship") && collidedObject != null)
+			{
+				isOnLandingPad = false;
+			}
 	}
 
 	public class Functions
@@ -189,77 +240,84 @@ public class project1 : ModBehaviour
 			float fractionaryFuel = shipResources.GetFractionalFuel(); // Get ship's fractional fuel
 			IsLanded = landingPadManager.IsLanded();    // Checks if ship is landed
 
+
 			// Refueling and gauges manager
-			// No fuel status
-			if (!IsLanded && fractionaryFuel > 0.3f)
+			if (!shipResources._hullBreach)
 			{
-				shipFuelGauge._indicatorLight.SetEmissionColor(Color.black); // Turn light off when not refueling
-			}
-			// Refueling
-			else if (IsLanded && !isOnLandingPad)
-			{
-				shipResources.SetFuel(shipResources._currentFuel += RefuelAmount);  // Increase fuel when landed
-				shipFuelGauge._indicatorLight.SetEmissionColor(Color.green);    //	Turn gauge green light on when refueling
-			}
-
-			// Refueling on landing pad
-			else if (IsLanded && isOnLandingPad)
-			{
-				shipResources.SetFuel(shipResources._currentFuel += RefuelAmount * 5);
-				shipFuelGauge._indicatorLight.SetEmissionColor(Color.magenta);
-			}
-
-			// Critical fuel level
-			else if (0f < fractionaryFuel && fractionaryFuel <= 0.3f && !IsLanded)
-			{
-				shipFuelGauge._indicatorLight.SetEmissionColor(Color.red); // Turn on warning light when low fuel
-			}
-
-			// Ship's notification manager
-			// No status
-			if (fractionaryFuel > 0.3)
-			{
-				if (NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+				// Refueling
+				if (enableRefuel && IsLanded)
 				{
-					NotificationManager.SharedInstance.UnpinNotification(criticalFuelNotification);
+					if (isOnLandingPad)
+					{
+						shipResources.SetFuel(shipResources._currentFuel += refuelAmount * landingPadRefuelMultiplier); // Multiply refuel if landed on landing pad
+						shipFuelGauge._indicatorLight.SetEmissionColor(Color.magenta);
+					}
+					else
+					{
+						shipResources.SetFuel(shipResources._currentFuel += refuelAmount);  // Increase fuel when landed
+						shipFuelGauge._indicatorLight.SetEmissionColor(Color.green);    //	Turn gauge green light on when refueling
+					}
 				}
-			}
 
-			// Fuel level critical
-			else if (0f < fractionaryFuel && fractionaryFuel <= 0.3f)
-			{
-				if (!NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+				// Gauges
+				// No fuel status
+				if (!IsLanded && fractionaryFuel > 0.3f)
 				{
-					NotificationManager.SharedInstance.PostNotification(criticalFuelNotification, true);
+					shipFuelGauge._indicatorLight.SetEmissionColor(Color.black); // Turn light off when not refueling
 				}
-			}
 
-			// Fuel depleted
-			else if (fractionaryFuel == 0)
-			{
-				if (NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+				// Critical fuel level
+				else if (0f < fractionaryFuel && fractionaryFuel <= 0.3f && !IsLanded)
 				{
-					NotificationManager.SharedInstance.UnpinNotification(criticalFuelNotification);
+					shipFuelGauge._indicatorLight.SetEmissionColor(Color.red); // Turn on warning light when low fuel
 				}
-			}
 
-			// Electrical systems & Refill station manager
-			if (shipResources._currentFuel <= 0f)
-			{
-				playerRecoveryPoint._refuelsPlayer = false; // Disables refueling when ship is out of fuel
-				mainElectricalSystem.SetPowered(false); // Disables electrical systems when ship is out of fuel
-			}
-			else if (shipResources._currentFuel > 0f && shipFuelGauge._shipDestroyed == false)
-			{
-				playerRecoveryPoint._refuelsPlayer = true;  // Activates refueling station in case of refueling
-				mainElectricalSystem.SetPowered(true);  // Enables electrical system in case of refueling
-			}
+				// Ship's notification manager
+				// No status
+				if (fractionaryFuel > 0.3)
+				{
+					if (NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+					{
+						NotificationManager.SharedInstance.UnpinNotification(criticalFuelNotification);
+					}
+				}
 
-			// Draw fuel to refill jetpack
-			bool isRefueling = playerResources.IsRefueling();
-			if (isRefueling && PlayerState.IsInsideShip() && shipResources._currentFuel > 0f)
-			{
-				shipResources._currentFuel -= 30f;
+				// Fuel level critical
+				else if (0f < fractionaryFuel && fractionaryFuel <= 0.3f)
+				{
+					if (!NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+					{
+						NotificationManager.SharedInstance.PostNotification(criticalFuelNotification, true);
+					}
+				}
+
+				// Fuel depleted
+				else if (fractionaryFuel == 0)
+				{
+					if (NotificationManager.SharedInstance.IsPinnedNotification(criticalFuelNotification))
+					{
+						NotificationManager.SharedInstance.UnpinNotification(criticalFuelNotification);
+					}
+				}
+
+				// Electrical systems & Refill station manager
+				if (shipResources._currentFuel <= 0f || shipResources._hullBreach)
+				{
+					playerRecoveryPoint._refuelsPlayer = false; // Disables refueling when ship is out of fuel
+					mainElectricalSystem.SetPowered(false); // Disables electrical systems when ship is out of fuel
+				}
+				else if (shipResources._currentFuel > 0f && !shipResources._hullBreach)
+				{
+					playerRecoveryPoint._refuelsPlayer = true;  // Activates refueling station in case of refueling
+					mainElectricalSystem.SetPowered(true);  // Enables electrical system in case of refueling
+				}
+
+				// Draw fuel to refill jetpack
+				bool isRefueling = playerResources.IsRefueling();
+				if (isRefueling && PlayerState.IsInsideShip() && shipResources._currentFuel > 30f)
+				{
+					shipResources._currentFuel -= 30f;
+				}
 			}
 		}
 		static public void OxygenManagement()
@@ -269,18 +327,22 @@ public class project1 : ModBehaviour
 			float fractionaryOxygen = shipResources.GetFractionalOxygen();  // Get fractional oxygen
 			bool isPlayerInShip = PlayerState.IsInsideShip();   // Check if player is inside ship
 
-			// Oxygen refill & gauge lights manager
+			// Oxygen regeneration
+			if (enableOxygenRegen)
+			{
+				// Refilling oxygen
+				if (isOxygenAvailable && enableOxygenRegen)
+				{
+					shipResources.SetOxygen(shipResources._currentOxygen += oxygenRefillAmount);
+					shipOxygenGauge._indicatorLight.SetEmissionColor(Color.green);
+				}
+			}
+			
+			// Gauges
 			// No oxygen status
 			if (!isOxygenAvailable && fractionaryOxygen > 0.33f)
 			{
 				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.black); // Turn light off when oxygen is not available
-			}
-
-			// Refilling oxygen
-			else if (isOxygenAvailable)
-			{
-				shipResources.SetOxygen(shipResources._currentOxygen += OxygenRefillAmount);    // Increase oxygen if it's available
-				shipOxygenGauge._indicatorLight.SetEmissionColor(Color.green);  // Turn gauge green light on when refilling
 			}
 
 			// Low oxygen
@@ -351,12 +413,43 @@ public class project1 : ModBehaviour
 				playerOxygenDetector._activeVolumes.Add(shipOxygenVolume);
 			}
 		}
+		static public void OxygenAsThrusters()
+		{
+			if (useOxygenAsFuel)
+			{
+				// Reduce oxygen when thrusting if fuel = 0
+				if (shipResources._currentFuel == 0f)
+				{
+					float magnitude = shipThrusterModel.GetLocalAcceleration().magnitude;
+					if (!isUsingEmergencyThrusters)
+					{
+						isUsingEmergencyThrusters = true;
+						NotificationManager.SharedInstance.PostNotification(shipResources._fuelDepletedNotification);
+					}
+					if (!shipOxygenDetector.GetDetectOxygen())
+					{
+						if (shipResources._currentOxygen > 0f)
+						{
+							float oxygenDecrease = magnitude * oxygenAsFuelMultiplier * Time.deltaTime;
+							shipResources._currentOxygen -= oxygenDecrease;
+						}
+					}
+				}
+				else
+				{
+					isUsingEmergencyThrusters = false;
+				}
+				if (!isUsingEmergencyThrusters && NotificationManager.SharedInstance.IsPinnedNotification(shipResources._fuelDepletedNotification))
+				{
+					NotificationManager.SharedInstance.UnpinNotification(shipResources._fuelDepletedNotification);
+				}
+			}
+		}
 	}
-
 	public IEnumerator ExitFlightConsoleDelay()
 	{
 		yield return new WaitForSeconds(8);
-		if (!isPlayerSeated)
+		if (!PlayerState.AtFlightConsole())
 		{
 			isSeatedExitDelay = false;
 		}
@@ -366,19 +459,32 @@ public class project1 : ModBehaviour
 	[HarmonyPatch]
 	public class HarmonyPatches
 	{
-		//	Create a patch to modify the amount of fuel used
+		// Override if thrusters are usable when out of fuel
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(ShipResources),nameof(ShipResources.AreThrustersUsable))]
+		public static void AreThrustersUsable_Postfix(ref bool __result, ShipResources __instance)
+		{
+			if (useOxygenAsFuel)
+			{
+				bool isOxygenAboveZero = __instance._currentOxygen > 0f;
+				bool isFuelAboveZero = __instance._currentFuel > 0f;
+				__result = isFuelAboveZero || isOxygenAboveZero;
+			}
+		}
+
+		// Create a patch to modify the amount of fuel used
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(ShipResources), nameof(ShipResources.DrainFuel))]
-		public static void ShipFuelUsageMultiplier(ref float amount)
+		public static void ShipResources_DrainFuel_Prefix(ref float amount)
 		{
 			// Calculate the increased fuel usage
 			amount *= 7f;
 		}
 
-			//Create a patch to increase oxygen consumption on the ship
+		// Create a patch to increase oxygen consumption on the ship
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(ShipResources), nameof(ShipResources.DrainOxygen))]
-		public static void ShipOxygenDrainMultiplier(ref float amount)
+		public static void ShipResources_DrainOxygen_Prefix(ref float amount)
 		{
 			//	Calculate increased oxygen drain
 			bool isOxygenAvailable = shipOxygenDetector.GetDetectOxygen();
@@ -393,15 +499,16 @@ public class project1 : ModBehaviour
 			}
 		}
 
+		// Create a patch to disable oxygen function when necessary
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(PlayerResources), nameof(PlayerResources.UpdateOxygen))]
-		public static bool PlayerResourcesUpdateOxygen_Prefix()
+		public static bool PlayerResources_UpdateOxygen_Prefix()
 		{
 			if (playerResources._currentOxygen == 0f && shipResources._currentOxygen == 0f && isPlayerSeated)
 			{
 				return true;
 			}
-			else if (isSeatedExitDelay && shipResources._currentOxygen != 0f && playerResources._currentOxygen != 0f)
+			else if (isSeatedExitDelay && shipResources._currentOxygen != 0f && playerResources._currentOxygen != 0f && !shipResources._hullBreach)
 			{
 				return false;
 			}
@@ -412,18 +519,24 @@ public class project1 : ModBehaviour
 			return true;
 		}
 
+		// Get when player enters flight console
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(PlayerState), nameof(PlayerState.OnEnterFlightConsole))]
-		public static void OnEnterFlightConsole_Postfix()
+		public static void PlayerState_OnEnterFlightConsole_Postfix()
 		{
 			isSeatedExitDelay = true;
 		}
 
+		// Get when player exits flight console
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(PlayerState), nameof(PlayerState.OnExitFlightConsole))]
-		public static void OnExitFlightConsole_Postfix()
+		public static void PlayerState_OnExitFlightConsole_Postfix()
 		{
 			Instance.StartCoroutine(Instance.ExitFlightConsoleDelay());
 		}
 	}
 }
+
+/// To do:
+/// Make functional temperature gauge
+/// Emergency oxygen thrusters [DONE!]
